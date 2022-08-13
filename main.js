d7f8c7dd2f5ad9ba1d01115b64d15dab1e66ee75
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-08-13 18:32:50 +0700

    show/close navbar

---
+++ main.js
@@ -0,0 +1,20 @@
+
+// show navbar
+function showNavbar() {
+  var x = document.getElementById("showMenu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// close navbar
+function closeNavbar() {
+  var x = document.getElementById("showMenu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
